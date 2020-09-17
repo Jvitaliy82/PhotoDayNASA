50be7bb1dbf4d9223704b981346ca953e0f89e14
Jvitaliy82 <jvit82@gmail.com>
2020-09-17 12:01:50 +0700

    add add progressBar and place holder

--- app/src/main/java/com/jdeveloperapps/photodaynasa/ui/main/MainFragment.kt
+++ app/src/main/java/com/jdeveloperapps/photodaynasa/ui/main/MainFragment.kt
@@ -1,5 +1,10 @@
 package com.jdeveloperapps.photodaynasa.ui.main
 
+import android.app.DatePickerDialog
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -7,17 +12,23 @@ import androidx.navigation.fragment.findNavController
 import coil.api.load
 import com.jdeveloperapps.photodaynasa.R
 import kotlinx.android.synthetic.main.main_fragment.*
+import java.util.*
 
 class MainFragment : Fragment(R.layout.main_fragment) {
 
     private lateinit var viewModel: MainViewModel
     private lateinit var hdUrl: String
 
+
     override fun onResume() {
         super.onResume()
+
+        setHasOptionsMenu(true)
+
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         val observer = Observer<PictureOfTheDayData> { renderData(it) }
         viewModel.getData().observe(viewLifecycleOwner, observer)
+        viewModel.sendServerRequest()
 
         image_view.setOnClickListener {
             val action = MainFragmentDirections.actionMainFragmentToDetailFragment(hdUrl)
@@ -34,13 +45,17 @@ class MainFragment : Fragment(R.layout.main_fragment) {
                 if (url.isNullOrEmpty()) {
                     //showDialog("Ошибка", "Сообщение с сервера пустое")
                 } else {
+                    showViewLoading(false)
                     message.text = serverResponseData.explanation
-                    image_view.load(url)
-                    hdUrl = serverResponseData.url!!
+                    image_view.load(url) {
+                        placeholder(R.drawable.ic_photo)
+                        error(R.drawable.ic_cancel)
+                    }
+                    hdUrl = serverResponseData.url
                 }
             }
             is PictureOfTheDayData.Loading -> {
-                //showViewLoading()
+                showViewLoading(true)
             }
             is PictureOfTheDayData.Error -> {
                 //showDialog("Ошибка!", data.error.message)
@@ -48,4 +63,39 @@ class MainFragment : Fragment(R.layout.main_fragment) {
         }
     }
 
+    private fun showViewLoading(visible: Boolean) {
+        if (visible) {
+            progressBar.visibility = View.VISIBLE
+        } else {
+            progressBar.visibility = View.GONE
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.main_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.calendar -> {
+                showCalendar()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
+    }
+
+    private fun showCalendar() {
+        DatePickerDialog(
+            requireContext(), { _, year, month, dayOfMonth ->
+                viewModel.setNewDate(year, month, dayOfMonth)
+                viewModel.sendServerRequest()
+            },
+            viewModel.getCurrentDate().get(Calendar.YEAR),
+            viewModel.getCurrentDate().get(Calendar.MONTH),
+            viewModel.getCurrentDate().get(Calendar.DAY_OF_MONTH)
+        ).show()
+    }
 }
--- app/src/main/java/com/jdeveloperapps/photodaynasa/ui/main/MainViewModel.kt
+++ app/src/main/java/com/jdeveloperapps/photodaynasa/ui/main/MainViewModel.kt
@@ -7,31 +7,36 @@ import com.jdeveloperapps.photodaynasa.data.api.PODRetrofitImpl
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainViewModel(
-        private val liveDataForViewToObserve: MutableLiveData<PictureOfTheDayData> = MutableLiveData(),
-        private val retrofitImpl: PODRetrofitImpl = PODRetrofitImpl()
+    private val liveDataForViewToObserve: MutableLiveData<PictureOfTheDayData> = MutableLiveData(),
+    private val retrofitImpl: PODRetrofitImpl = PODRetrofitImpl(),
+    private val currentDate: Calendar = Calendar.getInstance(),
+    private val simpleDateFormat: SimpleDateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
 ) : ViewModel() {
+
     fun getData(): LiveData<PictureOfTheDayData> {
-        sendServerRequest()
         return liveDataForViewToObserve
     }
 
-    private fun sendServerRequest() {
+    fun sendServerRequest() {
         liveDataForViewToObserve.value = PictureOfTheDayData.Loading(null)
-        retrofitImpl.getRetrofitImpl().getPictureOfTheDay("2020-09-09","DEMO_KEY").enqueue(object :
-                Callback<PODServerResponseData> {
+        val date = simpleDateFormat.format(currentDate.time)
+        retrofitImpl.getRetrofitImpl().getPictureOfTheDay(date, "DEMO_KEY").enqueue(object :
+            Callback<PODServerResponseData> {
 
             override fun onResponse(
-                    call: Call<PODServerResponseData>,
-                    response: Response<PODServerResponseData>
+                call: Call<PODServerResponseData>,
+                response: Response<PODServerResponseData>
             ) {
                 if (response.isSuccessful && response.body() != null) {
                     liveDataForViewToObserve.value =
-                            PictureOfTheDayData.Success(response.body()!!)
+                        PictureOfTheDayData.Success(response.body()!!)
                 } else {
                     liveDataForViewToObserve.value =
-                            PictureOfTheDayData.Error(Throwable("Unidentified error"))
+                        PictureOfTheDayData.Error(Throwable("Unidentified error"))
                 }
             }
 
@@ -41,16 +46,22 @@ class MainViewModel(
         })
     }
 
+    fun setNewDate(year: Int, month: Int, dayOfMonth: Int) {
+        currentDate.set(year, month, dayOfMonth)
+    }
+
+    fun getCurrentDate() = currentDate
+
 }
 
 data class PODServerResponseData(
-        val date: String?,
-        val explanation: String?,
-        val hdurl: String?,
-        val media_type: String?,
-        val service_version: String?,
-        val title: String?,
-        val url: String?
+    val date: String?,
+    val explanation: String?,
+    val hdurl: String?,
+    val media_type: String?,
+    val service_version: String?,
+    val title: String?,
+    val url: String?
 )
 
 sealed class PictureOfTheDayData {
